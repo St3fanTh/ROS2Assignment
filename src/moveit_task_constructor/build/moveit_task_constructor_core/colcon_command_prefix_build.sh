ece3b1cc8b659381cc5f34e34a323518a9654d93
Stefan Thich <thichstefan@gmail.com>
2025-07-14 22:21:26 +0200

    delete tutorials

--- src/moveit_task_constructor/build/moveit_task_constructor_core/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/login/ws_moveit/src/moveit_task_constructor/install/moveit_task_constructor_msgs/share/moveit_task_constructor_msgs/package.sh"
-. "/home/login/ws_moveit/src/moveit_task_constructor/install/rviz_marker_tools/share/rviz_marker_tools/package.sh"
--- src/moveit_task_constructor/build/moveit_task_constructor_visualization/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/ros/jazzy/src/gtest_vendor
-# Build directory: /home/login/ws_moveit/src/moveit_task_constructor/build/moveit_task_constructor_visualization/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
